ad592991af46a90ba1beae65166b4ee80f52b7b7
hemanthrachala <hemanthrachala@gmail.com>
2020-04-14 23:28:39 +0530

    created lrutest class

---
+++ lru/lrutest.py
@@ -0,0 +1,19 @@
+# from lru folder we should import lru
+from lru import lru
+
+class lrutest:
+    # constructor
+    def lrutest(self):
+        pass
+
+    # in the main method created a obj for main method
+    def main(self):
+        obj = lru(4)
+        obj.put("a")
+        
+
+if __name__ == "__main__":
+    obj = lrutest()        
+
+
+
