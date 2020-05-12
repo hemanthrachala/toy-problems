670a76054a74069473688f2bbf0b55e36015cfab
hemanthrachala <hemanthrachala@gmail.com>
2020-05-12 16:18:55 +0530

    delteing the task is done

--- todo _app/todo-app.js
+++ todo _app/todo-app.js
@@ -1,7 +1,10 @@
+let taskList = []
+
 class Task {
 
     constructor(name, dueDate, isDone) {
 
+        this.taskId = Date.now();
         this.name = name;
         this.dueDate= dueDate;
         this.isDone = isDone;
@@ -11,22 +14,43 @@ class Task {
         let htmlText= "<li>"+this.name
         htmlText += this.dueDate.getDate()+"/"+this.dueDate.getMonth();
         htmlText += '<input type="checkbox" name="isDone" id="isDone">'
+        htmlText += '<button onclick="deleteTask(';
+        htmlText += this.taskId;
+        htmlText += ')">DELETE</button>'
         htmlText += "</li>";
         return htmlText;
     }
 }
 
+
 function createTask(){
+
     const taskName = document.getElementById("taskName").value;
+
     addTask(new Task(taskName,new Date(), false))
 
 }
 
 function addTask(task){
 
+    taskList.push(task);
+
     const list = document.getElementById("todo")
 
     list.innerHTML += task.toString()
+}
+
+function deleteTask(taskId) {
+
+    taskList = taskList.filter(
+
+        (t) => {
+            if(t.taskId != taskId)
+            return t;
+        }
+    );
+    console.log(taskList);
+
 }
 
 
