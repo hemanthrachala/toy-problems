e74f7dfd08df3d4781f0668194cdf13350dc06a2
hemanthrachala <hemanthrachala@gmail.com>
2020-05-17 18:16:07 +0530

    done with list printing part

--- todoreact/todo-app.js
+++ todoreact/todo-app.js
@@ -1,4 +1,45 @@
+function Task(props) {
+return <li>{props.name}, {new Date().toLocaleTimeString()}</li>
+}
+
+class TodoList extends React.Component {
+
+  addTask() {
+    console.log("add Task");
+  }
+
+  render() {
+
+    const TaskList = this.props.list.map((t) =>
+        <Task name = {t.name} />
+    );
+
+    return (
+      <div>
+        <h1>TODO</h1>
+        <ol>
+          {TaskList}
+        </ol>
+        <button onClick = {this.addTask}>
+          ADD TASK
+        </button>
+      </div>
+
+    );
+      
+  }
+
+}
+
+const tasks = [
+  {name: "Welcome task"}, 
+  {name: "first task"},
+  {name: "second task"},
+  {name: "third task"}]
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <TodoList list={tasks}/>,
     document.getElementById('todo')
   );
+
+
+
